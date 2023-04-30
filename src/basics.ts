8e6aeaeff946a3c256951cb6fc5897279fe61cc2
yohanessilitonga <yohanesrfsilitonga21@gmail.com>
2023-04-30 22:44:38 +0800

    adding type func-and-func

--- src/basics.ts
+++ src/basics.ts
@@ -26,3 +26,27 @@ ids[30] = 'c';
 console.log(ids);
 
 const result = [1, 2, 3, 4].map((v) => v);
+
+const fetchData = (url: string): Promise<string> =>
+  Promise.resolve('Data from url');
+
+const printToFile = (text: string, callback: () => void): void => {
+  console.log(text);
+  callback();
+};
+
+type MutationFunction = (v: number) => number;
+
+const mutateArray = (numbers: number[], mutate: MutationFunction): number[] => {
+  return numbers.map(mutate);
+};
+
+console.log(mutateArray([1, 20, 3], (v) => v * 10));
+
+type AdderFunction = (num: number) => number;
+
+const createAdder = (num: number): AdderFunction => {
+  return (newNum: number) => newNum + num;
+};
+
+const addingOne = createAdder(1);
