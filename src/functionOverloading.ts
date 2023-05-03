ed55b3ae340c0b6a4e0977ff5d3b6ead638bdf96
yohanessilitonga <yohanesrfsilitonga21@gmail.com>
2023-05-03 23:35:12 +0800

    adding function overloading

--- src/functionOverloading.ts
+++ src/functionOverloading.ts
@@ -5,6 +5,7 @@ interface Coord {
 
 function parseCoordinate(obj: Coord): Coord;
 function parseCoordinate(x: number, y: number): Coord;
+function parseCoordinate(strObj: string): Coord;
 
 function parseCoordinate(arg1: unknown, arg2?: unknown): Coord {
   let newCoord: Coord = {
@@ -12,7 +13,12 @@ function parseCoordinate(arg1: unknown, arg2?: unknown): Coord {
     y: 0,
   };
 
-  if (arg1 instanceof Object) {
+  if (typeof arg1 === 'string') {
+    arg1.split(',').forEach((str) => {
+      const [key, value] = str.split(':');
+      newCoord[key as 'x' | 'y'] = parseInt(value, 10);
+    });
+  } else if (arg1 instanceof Object) {
     newCoord = arg1 as Coord;
   } else {
     newCoord = {
@@ -26,3 +32,4 @@ function parseCoordinate(arg1: unknown, arg2?: unknown): Coord {
 
 console.log(parseCoordinate(1, 2));
 console.log(parseCoordinate({ x: 1, y: 2 }));
+console.log(parseCoordinate('x:12,y:22'));
