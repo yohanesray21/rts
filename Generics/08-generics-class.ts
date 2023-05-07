3bb3a13e47b155f1a2cc9c1ca66b212cdf8985bf
yohanessilitonga <yohanesrfsilitonga21@gmail.com>
2023-05-07 15:36:40 +0800

    class extends and implements using typescript

--- Generics/08-generics-class.ts
+++ Generics/08-generics-class.ts
@@ -1,21 +1,45 @@
 interface Database {
-  get: (value: string) => string;
+  get: (id: string) => string;
   set: (id: string, value: string) => void;
 }
 
-class myDatabase implements Database {
-  private db: Record<string, string> = {};
-
+class InMemoryDatabase implements Database {
+  protected db: Record<string, string> = {};
   get(id: string): string {
     return this.db[id];
   }
-  set(id: string, value: string): void {
+  set(id: string, value: string) {
     this.db[id] = value;
   }
 }
 
-const myDB = new myDatabase();
-myDB.set('foo', 'bar');
-console.log(myDB.get('foo'));
-myDB.db['foo'] = 'baz';
-console.log(myDB.get('foo'));
+// const myDatabase = new InMemoryDatabase();
+// myDatabase.set('foo', 'barr');
+// console.log(myDatabase.get('foo'));
+// myDatabase.db['foo'] = 'nez';
+// console.log(myDatabase.get('foo'));
+
+interface Persitable {
+  saveToString: () => string;
+  restoreFromString: (storeState: string) => void;
+}
+
+class PersistableDB extends InMemoryDatabase implements Persitable {
+  saveToString() {
+    return JSON.stringify(this.db);
+  }
+  restoreFromString(value: string) {
+    this.db = JSON.parse(value);
+  }
+}
+
+const myPersistable = new PersistableDB();
+myPersistable.set('foo', 'barr');
+console.log(myPersistable.get('foo'));
+const saved = myPersistable.saveToString();
+myPersistable.set('foo', 'db-1');
+console.log(myPersistable.get('foo'));
+
+const myDB2 = new PersistableDB();
+myDB2.restoreFromString(saved);
+console.log(myDB2.get('foo'));
